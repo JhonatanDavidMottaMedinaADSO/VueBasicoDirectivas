5bfa52b94e061a7063d46ae3c11249e99959ef72
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2023-03-21 16:42:59 -0500

    feat: :sparkles: v-text
    Usamos la directiva v-text

--- components/component.js
+++ components/component.js
@@ -1,7 +1,9 @@
 export default {
     data() {
         return {
-            mensaje:"Hello World"
+            basico: "Apolo",
+            intermedio: "Artemid",
+            avanzado: "Sputnik"
         }
     }
 }
