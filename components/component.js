2688efaf8d95657ad859b716e98e396c1295c3f6
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2023-03-21 23:27:32 -0500

    feat: :sparkles: v-model
    Se hizo uso de la directiva v-model

--- components/component.js
+++ components/component.js
@@ -1,9 +1,7 @@
 export default {
     data() {
         return {
-            basico: `<strong style="color: green;">Apolo</strong>`,
-            intermedio: `<strong style="color: orange;">Artemid</strong>`,
-            avanzado: `<strong style="color: blue;">Sputnik</strong>`
+            mensaje: "lorem"
         }
     }
 }
